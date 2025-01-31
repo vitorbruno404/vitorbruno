0061cf90e3d49d3ea6f5f30209a296ccb194c686
vitorbruno404 <vitor.bruno@vitorbruno.com>
2025-01-31 13:27:51 +0100

    init commit

---
+++ .anaconda/navigator/scripts/jupyterlab.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /opt/anaconda3/bin/activate /opt/anaconda3
+jupyter lab >/Users/vitorbruno/.anaconda/navigator/scripts/jupyterlab-out-1.txt 2>/Users/vitorbruno/.anaconda/navigator/scripts/jupyterlab-err-1.txt
